8f263e9958103face22abb2ec360a3ee7dfca913
Sean Ryan <str_ie@yahoo.co.uk>
2012-03-24 23:02:04 +0000

    processor now registers with nameserver + gets model from broker

--- broker.py
+++ broker.py
@@ -3,12 +3,17 @@
 import Pyro.core
 import Pyro.naming
 
+from region import *
+
 class Broker(Pyro.core.ObjBase):
 	def __init__(self):
 		Pyro.core.ObjBase.__init__(self)
-		self.nextPID = 0        	
+		self.nextPID = 0        
+		self.model = RegionModel.Load("regions.csv", "region_connections.csv")
 	def register(self): #returns new PID for the client
 		return self.getNextPID()
+	def getModel(self):
+		return self.model
 	def getNextPID(self):
 		self.nextPID = self.nextPID + 1
 		return self.nextPID
@@ -23,3 +28,4 @@ def main():
 
 if __name__=="__main__":
     main()
+
--- processor.py
+++ processor.py
@@ -2,14 +2,44 @@
 
 import Pyro.core
 
-#use the pyro nameserver to locate the broker
-broker = Pyro.core.getProxyForURI("PYRONAME://broker")
+class Processor(Pyro.core.ObjBase):
+	def __init__(self):
+		Pyro.core.ObjBase.__init__(self)
+		self._register()
 
-myPID = broker.register()
+	def _register(self):
+		#use the pyro nameserver to locate the broker
+		self.broker = Pyro.core.getProxyForURI("PYRONAME://broker")
 
-print "my PID = " + str(myPID)
+		self.myPID = self.broker.register()
+		print "my PID = " + str(self.myPID)
 
-#now register with the nameserver, so that newer processors can find me
+	 	self.model = self.broker.getModel()
+		#todo copy the model locally
+
+		print "retrieved model from broker: "
+		print self.model.toString()
+
+
+def main():
+	proc = Processor()	
+
+	#todo start a new thread, to perform the traffic algorithm
+
+	#register this proc, so that other procs can reach it:
+	Pyro.core.initServer()
+	ns=Pyro.naming.NameServerLocator().getNS()
+	daemon=Pyro.core.Daemon()
+	daemon.useNameServer(ns)
+	#now register with the nameserver, so that newer processors can find me
+	uri=daemon.connect(proc,"proc" + str(proc.myPID))
+	#server thread, that processes requests:
+	daemon.requestLoop()
+
+if __name__=="__main__":
+    main()
+
+
 
 #take work off the other existing processors
 
@@ -28,4 +58,12 @@ print "my PID = " + str(myPID)
 #give my regions to the other processors
 
 #exit
+
+
+
+
+
+
+
+
 
